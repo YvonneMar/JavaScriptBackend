5ad6360eb2871a579e40f4f126f3e3d245af222b
Rocio Martinez <rmartinez@polyworksmexico.com>
2023-09-08 17:37:31 -0600

    Desafio 1 correción

--- main.js
+++ main.js
@@ -1,16 +1,27 @@
 class ProductManager {
     constructor() {
         this.products = []
+        this.productIdCounter = 1;
     }
 
-    addProduct(product) {
-        const prod = this.products.find(prod => prod.code === product.code)
-
-        if (prod) {
-            console.log("Producto ya encontrado")
-        } else {
-            this.products.push(product)
+    addProduct(productData) {
+        if (this.products.some((product) => product.code === productData.code)) {
+          throw new Error('Producto con el mismo código ya existe');
         }
+
+        const requiredFields = ['title', 'description', 'price', 'thumbnail', 'code', 'stock'];
+        for (const field of requiredFields) {
+        if (!productData[field]) {
+            throw new Error(`${field} is a mandatory field.`);
+        }
+        }
+
+        const newProduct = {
+            id: this.productIdCounter++,
+            ...productData,
+        };
+    
+        this.products.push(newProduct);
     }
 
     getProducts() {
@@ -18,12 +29,56 @@ class ProductManager {
     }
 
     getProductById(id) {
-        const prod = this.products.find(prod => prod.id === id)
-
-        if (prod) {
-            console.log(prod)
-        } else {
-            console.log("Producto no encontrado")
+        const product = this.products.find((product) => product.id === id);
+        if (!product) {
+          console.log('Not found');
         }
-    }
+        return product;
+      }
 }
+
+//Prueba:
+const productManager = new ProductManager();
+
+productManager.addProduct({
+    title: 'Product 1',
+    description: 'Description 1',
+    price: 10.99,
+    thumbnail: 'product1.jpg',
+    code: 'P001',
+    stock: 100,
+  });
+
+productManager.addProduct({
+    title: 'Product 2',
+    description: 'Description 2',
+    price: 19.99,
+    thumbnail: 'product2.jpg',
+    code: 'P002',
+    stock: 50,
+});
+
+productManager.getProducts()
+
+//id no existente
+//productManager.getProductById(3); 
+
+//Falta campo stock
+    // productManager.addProduct({
+    // title: 'Product 3', //Faltan campos
+    // description: 'Description 3',
+    // price: 7.99,
+    // thumbnail: 'product3.jpg',
+    // code: 'P003', //Campo code repetido
+// });
+
+//Campo code repetido:
+// productManager.addProduct({
+    // title: 'Product 4',
+    // description: 'Description 4',
+    // price: 5.99,
+    // thumbnail: 'product5.jpg',
+    // code: 'P002', 
+    // stock: 50
+// });
+    
